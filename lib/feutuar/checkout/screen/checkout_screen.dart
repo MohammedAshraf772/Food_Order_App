62116d396f8076cf589fb1af327e375fe555fea2
MohammedAshraf772 <mohamed117ashraf@gmail.com>
2026-07-11 17:58:51 +0300

    cash & visa

--- lib/feutuar/checkout/screen/checkout_screen.dart
+++ lib/feutuar/checkout/screen/checkout_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_order_app/core/constants/app_colors.dart';
 import 'package:food_order_app/feutuar/checkout/widget/order_details_widget.dart';
+import 'package:food_order_app/shared/custame_button.dart';
 import 'package:food_order_app/shared/custame_text.dart';
 import 'package:gap/gap.dart';
 
@@ -12,6 +12,7 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
+  String selectMethod = 'cash';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,16 +47,94 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
             ),
             Gap(20),
             ListTile(
-              leading: Image.asset("assets/test/dollar.png", width: 50),
-              title: Text("cash on Delivery"),
+              onTap: () {
+                setState(() => selectMethod = 'cash');
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(8),
+              ),
+              tileColor: Color(0xff3C2F2F),
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+              leading: Image.asset(
+                "assets/test/dollar.png",
+                width: 50,
+                height: 35,
+              ),
+              title: CustameText(text: "cash on Delivery", color: Colors.white),
               trailing: Radio<String>(
-                activeColor: AppColors.primaryColor,
+                activeColor: Colors.white,
                 value: "cash",
-                groupValue: "Cash",
-                onChanged: (v) {},
+                groupValue: selectMethod,
+                onChanged: (v) {
+                  setState(() {
+                    selectMethod = v!;
+                  });
+                },
               ),
             ),
+            Gap(15),
+            ListTile(
+              onTap: () {
+                setState(() => selectMethod = 'visa');
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(8),
+              ),
+              tileColor: Colors.blue.shade900,
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+              leading: Image.asset(
+                "assets/test/visa-logo.png",
+                width: 50,
+                height: 50,
+              ),
+              title: CustameText(text: "Debit card", color: Colors.white),
+              subtitle: CustameText(
+                text: '**** **** **** 2536',
+                color: Colors.white,
+              ),
+              trailing: Radio<String>(
+                activeColor: Colors.white,
+                value: "visa",
+                groupValue: selectMethod,
+                onChanged: (v) {
+                  setState(() {
+                    selectMethod = v!;
+                  });
+                },
+              ),
+            ),
+            Gap(5),
+            Row(
+              children: [
+                Checkbox(
+                  activeColor: Colors.red,
+                  value: true,
+                  onChanged: (v) {},
+                ),
+                CustameText(text: "save card details future payments"),
+              ],
+            ),
           ],
+        ),
+      ),
+      bottomSheet: Container(
+        height: 120,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustameText(text: "Total", size: 30),
+                  CustameText(text: "\$18.9", size: 30),
+                ],
+              ),
+              CustameButton(text: "Add To Cart", ontap: () {}),
+            ],
+          ),
         ),
       ),
     );
